09a04dc22ecc4e8d24995d6fe0814b4bef510870
Gerwald Gindra-Vady <gerwald.gindra-vady@edu.fh-joanneum.at>
2023-01-23 19:25:57 +0100

    main class major rework

--- src/main/java/at/fhj/iit/CalculatorRunner.java
+++ src/main/java/at/fhj/iit/CalculatorRunner.java
@@ -23,7 +23,8 @@ public class CalculatorRunner {
                 System.exit(0);
                 break;
         }
-        System.out.println("\nCalculation finished\n");
+        System.out.println("-----------------------");
+        System.out.println("Calculation finished\n");
         System.out.println(String.format("Result of Calculator: %,.2f %s %,.2f is %,.2f", number1, operation, number2, result));
     }
 
